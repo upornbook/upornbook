2075e02bb5e6e7ec1e58a3e8096f77e4e917e4c7
upornbook <upbookinfo@gmail.com>
2019-09-11 21:36:54 +0000

    commiting new posts

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'upornbook_db' );
+define( 'DB_NAME', 'taha' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'badboy101' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
